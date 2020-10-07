e55c290e56c42b6dda473e91c1522486ec6ee3bd
Manish Poduval <manishpoduval@hotmail.com>
2020-10-07 15:20:27 +0530

    Handlebars complete

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 let {students, instructors} = require('./data')
 
@@ -12,9 +14,43 @@ app.use((req, res, next) => {
 
 app.use(express.static(__dirname + "/public"));
 
+const hbs = require('hbs')
+
+hbs.registerPartials(__dirname + "/views/partials")
+
+app.set('view engine', 'hbs');
+
+// views only works with dynamic files 
+app.set('views', __dirname + '/views' )
+
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/views/landing.html')
+    res.render('landing.hbs' , { name: 'Manish', layout: false } )
 });
+
+app.get("/students", (req, res) => {
+    students.sort((a, b) => {
+        if (a.name > b.name) {
+            return 1
+        }
+        else if (a.name < b.name) {
+            return -1
+        }
+        else {
+            return 0
+        }
+    })
+    students = students.map((student) => {
+        student.name = student.name.toUpperCase()
+        return student
+    })
+    res.render('students.hbs' , {students})
+});
+
+app.get("/instructors", (req, res) => {
+    res.render('instructors.hbs', {instructors})
+});
+
 
 app.listen(port, () =>
     console.log(`Example app listening at http://localhost:${port}`)
